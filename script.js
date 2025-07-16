6963720cd9864a7466e7bd57645d97e7e9844ac7
ronmor76 <ronen.mordechai@gmail.com>
2025-07-16 21:17:45 +0300

    Create new closure for post likes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 alert("hellooo")
+
+function scoping(postName){
+    let likes = 0;
+
+    // closure
+    return function(){
+        likes++;
+    }
+}
+
+let likesPost1 = scoping("Post Fight")
+likesPost1();
+likesPost1();
